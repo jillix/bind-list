e0c8291b6e71389db516932f1b8042263e693e75
Adrian <adi@ottiker.com>
2012-06-04 18:40:16 +0200

    get data from db

--- operations.js
+++ operations.js
@@ -1,64 +1,33 @@
 var send = require(CONFIG.root + "/core/send.js").send;
-var data = [
-    
-    {
-        val: "Domain 1",
-        events: {
-            
-            mouseup: {
-                
-                method: "event",
-                args: ["vertex_detail", "showDetails", {name: "domain1.com", application: "#11:0"}]
-            }
-        }
-    },
-    {
-        val: "Domain 2",
-        events: {
-            
-            mouseup: {
-                
-                method: "event",
-                args: ["vertex_detail", "showDetails", {name: "domain2.com", application: "#11:1"}]
-            }
-        }
-    },
-    {
-        val: "Domain 3",
-        events: {
-            
-            mouseup: {
-                
-                method: "event",
-                args: ["vertex_detail", "showDetails", {name: "domain3.com", application: "#11:2"}]
-            }
-        }
-    },
-    {
-        val: "Domain 4",
-        events: {
-            
-            mouseup: {
-                
-                method: "event",
-                args: ["vertex_detail", "showDetails", {name: "domain4.com", application: "#11:3"}]
-            }
-        }
-    },
-    {
-        val: "Domain 5",
-        events: {
-            
-            mouseup: {
-                
-                method: "event",
-                args: ["vertex_detail", "showDetails", {name: "domain5.com", application: "#11:4"}]
-            }
-        }
-    },
-];
 
 exports.getData = function(link) {
     
-    send.ok(link.res, data);
+    CONFIG.orient.DB.command(link.params.query, function(err, result) {
+        
+        if (err) {
+            
+            send.internalservererror(link.res, err);
+            return;
+        }
+        
+        var data = [];
+        
+        for (var i = 0, l = result.length; i < l; ++i) {
+            
+            data.push({
+                
+                val: result[i][link.params.nameField],
+                events: {
+            
+                    mouseup: {
+                        
+                        method: "event",
+                        args: [link.params.obsName, link.params.eventName, result[i]]
+                    }
+                }
+            });
+        }
+        
+        send.ok(link.res, data);
+    });
 };
