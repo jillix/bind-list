ddbbfc8945bcc9b860349a9758204244af8ed033
Gabriel Petrovay <gabipetrovay@gmail.com>
2012-10-12 09:44:37 +0200

    bind list skeleton

--- main.js
+++ main.js
@@ -1,59 +1,12 @@
-define(["adioo/bind/repeater"], function(Repeater) {
-    
-    var List = {
-        
-        event: function(miid, eventName, data) {
-            
-            N.obs(miid).f(eventName, data);
-        }
-    };
-    
-    /*
-        config = {
-            
-            target: "#selector",
-            source: {},
-            bind: [{}]
-        }
-        
-        
-    i18n: false, //true is default,
-    addItem: "#addItemButton",
-    removeItem: "#removeItemButton",
-    paging: 33,
-    search: [
-        {
-            elm: "#searchField",
-            ??
-        }
-    ]
-    
-    // TODO:
-    - locale change
-    - add item
-    - remove item
-    - paging
-    - search data
-    */
-    
+define(["github/adioo/bind-list/v0.1.1/list"], function(List) {
+
     function init(config) {
-        
-        var list = N.ext(List, Repeater(this), config);
-        
-        list.target = list.dom.querySelector(config.target);
-        
-        if (list.source) {
-            
-            list.fetch();
-        }
-        
-        if (list.bind) {
-            
-            list.render(list.bind);
-        }
-        
-        return list;
+
+        var list = Object.extend(List, this);
+
+        alert("list initialized");
     }
     
     return init;
 });
+
--- operations.js
+++ operations.js
@@ -1,33 +1,17 @@
 var send = require(CONFIG.root + "/core/send.js").send;
 
-exports.getData = function(link) {
-    
-    CONFIG.orient.DB.command(link.params.query, function(err, result) {
-        
-        if (err) {
-            
-            send.internalservererror(link.res, err);
-            return;
-        }
-        
-        var data = [];
-        
-        for (var i = 0, l = result.length; i < l; ++i) {
-            
-            data.push({
-                
-                val: result[i][link.params.nameField],
-                events: {
-            
-                    mouseup: {
-                        
-                        method: "event",
-                        args: [link.params.obsName, link.params.eventName, result[i]]
-                    }
-                }
-            });
-        }
-        
-        send.ok(link.res, data);
-    });
+exports.create = function(link) {
+    send.ok(link.res, { status: "OK" });
+};
+
+exports.read = function(link) {
+    send.ok(link.res, { status: "OK" });
+};
+
+exports.update = function(link) {
+    send.ok(link.res, { status: "OK" });
+};
+
+exports.remove = function(link) {
+    send.ok(link.res, { status: "OK" });
 };
